b0d4dcf3f93d8721d9575253bd67dfcf8d81c287
area268457913 <area268457913@hotmail.com>
2021-11-09 15:11:33 +0800

    add admin restaurant indexpage

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
-
+const adminController = require('../controllers/adminController.js')
+const { get } = require('superagent')
 
 module.exports = (app) => {
 
@@ -7,4 +8,8 @@ module.exports = (app) => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
   //在 /restaurants 底下則交給 restController.getRestaurants 來處理
   app.get('/restaurants', restController.getRestaurants)
+  // 連到 /admin 頁面就轉到 /admin/restaurants
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
+  app.get('/admin/restaurants', adminController.getRestaurants)
 }
