14a4d4d3ef52db6d64edd71c3fdff49390605928
area268457913 <area268457913@hotmail.com>
2021-11-13 22:13:29 +0800

    adminController.getUser & adminController.toggleAdmin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,6 +3,7 @@ const Restaurant = db.Restaurant
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const User = db.User
 
 const adminController = {
   getRestaurants: (req, res) => {
@@ -122,8 +123,28 @@ const adminController = {
             res.redirect('/admin/restaurants')
           })
       })
-  }
+  },
 
+  getUsers: (req, res) => {
+    return User.findAll({ raw: true }).then(users => {
+      return res.render('admin/users', { users: users })
+    })
+
+  },
+  toggleAdmin: (req, res) => {
+    return User.findByPk(req.params.id).then((user) => {
+      const { isAdmin, email } = user.toJSON();
+      if (email === "root@example.com") {
+        req.flash("error_messages", "禁止變更管理者權限");
+        return res.redirect("back");
+      } else {
+        user.update({ isAdmin: !isAdmin }).then((user) => {
+          req.flash("success_messages", "使用者權限變更成功");
+          res.redirect("/admin/users");
+        });
+      }
+    });
+  },
 }
 
 module.exports = adminController
