390e073ff2132c7fd962e7cd2144ad06c2d7d2f2
area268457913 <area268457913@hotmail.com>
2021-11-16 02:35:05 +0800

    Create category model & Add restaurant.CategoryId and association

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Restaurant.belongsTo(models.Category)  // 加入關聯設定
     }
   };
   Restaurant.init({
@@ -19,7 +20,9 @@ module.exports = (sequelize, DataTypes) => {
     address: DataTypes.STRING,
     opening_hours: DataTypes.STRING,
     description: DataTypes.TEXT,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
+    CategoryId: DataTypes.INTEGER  // 更新欄位清單
+
 
   }, {
     sequelize,
