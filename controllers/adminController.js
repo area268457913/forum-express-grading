a984021642035a67b1881fe0be4522eca25d1385
area268457913 <area268457913@hotmail.com>
2021-11-16 03:59:41 +0800

    Show category name on admin/restaurants & Show category name on admin/restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,12 +2,20 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const fs = require('fs')
 const imgur = require('imgur-node-api')
+const category = require('../models/category')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const User = db.User
+const Category = db.Category
+
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true }).then(restaurants => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurants => {
+      // console.log(restaurants)
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
   },
@@ -54,10 +62,11 @@ const adminController = {
     }
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(
+    return Restaurant.findByPk(req.params.id, { include: [Category] }).then(
       restaurant => {
+        console.log(restaurant)
         return res.render('admin/restaurant', {
-          restaurant: restaurant
+          restaurant: restaurant.toJSON()
         })
       }
     )
