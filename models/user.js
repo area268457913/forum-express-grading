89a0d9c12b2a51afa96d69d8b1f62b0c67aa616f
area268457913 <area268457913@hotmail.com>
2021-11-22 01:03:29 +0800

    Create followship model and add followers and followings to users

--- models/user.js
+++ models/user.js
@@ -21,6 +21,11 @@ module.exports = (sequelize, DataTypes) => {
           through: models.Like,
           foreignKey: 'UserId',
           as: 'LikeRestaurants'
+        }),
+        User.belongsToMany(User, {
+          through: models.Followship,
+          foreignKey: 'followingId',
+          as: 'Followers'
         })
     }
   };
