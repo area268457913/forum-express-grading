af36df3e18c18a07311e6035f80cdd2bdbbdda2d
area268457913 <area268457913@hotmail.com>
2021-11-21 16:32:21 +0800

    add viewCounts

--- controllers/restController.js
+++ controllers/restController.js
@@ -61,6 +61,7 @@ const restController = {
       ]
     }).then(restaurant => {
       // console.log(restaurant.Comments[0].dataValues)
+      restaurant.increment("viewCounts");
       return res.render('restaurant', {
         restaurant: restaurant.toJSON(),
       })
